99cb360acd2bead0362dabc75bfa69a9efe652e9
DaviiiiiiiFialho <105887788+DaviiiiiiiFialho@users.noreply.github.com>
2024-08-21 07:39:40 -0300

    s

---
+++ script.js
@@ -0,0 +1,52 @@
+const caixaPrinciapl = document.querySelector('.caixa-principal');
+const caixaPrinciapl = document.querySelector('.caixa-perguntas');
+const caixaPrinciapl = document.querySelector('.caixa-alternativas');
+const caixaPrinciapl = document.querySelector('.caixa-resultado');
+const caixaPrinciapl = document.querySelector('.texto-resultado');
+
+const alternativas = [
+    {
+        enunciado: "No ambito social, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 1",
+                afirmacao: "Afirmação 1"
+            },
+            {
+                texto: "Alternativa 2",
+                    afirmacao: "Afirmação 2"
+            }
+        ]
+
+    },
+    {
+        enunciado: "No ambito ambiental, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 1",
+                afirmacao: "Afirmação 1"
+            },
+            {
+                texto: "Alternativa 2",
+                    afirmacao: "Afirmação 2"
+            }
+        ]
+
+    },
+    {
+        enunciado: "No ambito tecnologico, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 1",
+                afirmacao: "Afirmação 1"
+            },
+            {
+                texto: "Alternativa 2",
+                    afirmacao: "Afirmação 2"
+            }
+        ]
+
+    },
+]
+
+    
